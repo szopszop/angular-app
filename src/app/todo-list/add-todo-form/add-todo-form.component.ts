496f88bcf1747deddd70eecbed3fd6c45a513d7f
szymon tracz <s.tracz@blachotrapez.com.pl>
2023-12-25 14:54:48 +0100

    add-todo-form and todo components separation

---
+++ src/app/todo-list/add-todo-form/add-todo-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo-form',
+  templateUrl: './add-todo-form.component.html',
+  styleUrls: ['./add-todo-form.component.css']
+})
+export class AddTodoFormComponent {
+
+}
